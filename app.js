6b32382d561771e77c1fe8657d265ef03a505dd9
Candela <candelaneirotti@hotmail.com>
2022-05-24 15:31:56 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+
+        const menu = document.getElementById ("abrir-menu");
+        menu.addEventListener("click",() =>{
+            document.getElementById("contenedor-menu").classList.toggle("active");
+        })
+
+
+
+        
+
